19282a45919d7d65034910dfa6e1fbc47f246548
palmierie <ericmpalmieri@gmail.com>
2017-07-19 21:10:51 -0500

    just need to add the right half of the pyramid now

--- main.js
+++ main.js
@@ -1,30 +1,59 @@
 let tree = [];
 
-var leaf;
+var leaf = [];
 
 var chooseHeight;
 
-var clicked = document.getElementById("submitButton");
- clicked.addEventListener("click", function(){
-  // Pull input from user
-  let chooseHeightString = document.getElementById("height-input").value;
-  leaf = document.getElementById("char-input").value;
+var spaces = "X";
+var treeArray =[];
+var realTree = [];
+var realLeaf = leaf;
 
-  // Change input from string into number
-  chooseHeight = parseInt(chooseHeightString);
+var clicked = document.getElementById("submitButton");
+var entered = document.getElementById("form");
+
+    function pullInput() {
+
+      // ADD CONDITIONAL FOR NO ENTRY HERE
+
+      // Pull input from user
+      let chooseHeightString = document.getElementById("height-input").value;
+      leaf = document.getElementById("char-input").value;
+
+      // Change input from string into number
+      chooseHeight = parseInt(chooseHeightString);
+      
+      console.log('height', chooseHeight, "Character", leaf);
+      
+      for (var i = 0; i < chooseHeight; i++){
+        debugger;
+        tree += leaf;
+        var bigTree = (tree.length * 2) - 1;
+        var spaceArray = [];
+        var outI = i;
+      for( var n = chooseHeight - outI - 1; n > 0 ; n--){
+        spaceArray += spaces;
+      }
+      for (var w = 0; w < bigTree; w++){
+        realTree += realLeaf;
+      }
+      // treeArray = spaceArray + tree;
+      treeArray = spaceArray + realTree;
+      console.log('Tree Array', treeArray);
+      }
+    }
+
+// USER CLICKS OR PRESSES ENTER
+clicked.addEventListener("click", pullInput);
+form.addEventListener("keyup", function(event){
+  if (event.which === 13){
+    pullInput();
+  }
 });
 
-function runTree(height,character) {
-  for (let i = 0; i < height; i++){
-    let n = i + 1;
-    tree = character * n;
-  }
-}
 
-// Check Input
-function checkInput() {
-  
-}
+
+/*  CONDITIONAL
 if (chooseHeight === ""|| leaf === ""){
   alert("You must enter a number in both fields");
   console.log("did not work");
@@ -32,8 +61,7 @@ if (chooseHeight === ""|| leaf === ""){
 } else {
   console.log('height & char', chooseHeight, leaf);
   // Run Program Function
-  runTree(chooseHeight,leaf);
   
 }
 
-
+*/
